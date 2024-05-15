1b4d527af56e93be31b842513d30e8a8bee77477
AurelienAllenic <aurelien.allenic@gmail.com>
2024-05-15 15:51:57 +0200

    (feat) added creation of events with contract associated so that every possibility for manager works

--- epic_events/epic_events_CRM/initializer.py
+++ epic_events/epic_events_CRM/initializer.py
@@ -9,26 +9,25 @@ from django.contrib.auth.hashers import make_password
 from crm.models import Collaborator, Client, Role, Contract, Evenement
 from datetime import date
 
-# Réinitialiser la base de données avant de relancer la création des gourpes et users
+# Réinitialiser la base de données avant de relancer la création des groupes et utilisateurs
 django.db.connections['default'].cursor().execute('SET FOREIGN_KEY_CHECKS=0')
 Collaborator.objects.all().delete()
 Client.objects.all().delete()
 Group.objects.all().delete()
+Contract.objects.all().delete()
+Evenement.objects.all().delete()
 django.db.connections['default'].cursor().execute('SET FOREIGN_KEY_CHECKS=1')
 
-
-# Create groups
 group_names = ['management_team', 'sales_team', 'support_team']
 groups = {}
 for group_name in group_names:
     group, created = Group.objects.get_or_create(name=group_name)
     if created:
-        print(f"Group '{group_name}' created .")
+        print(f"Group '{group_name}' created.")
     else:
         print(f"Group '{group_name}' already exists.")
     groups[group_name] = group
 
-# Assign permissions to each group
 permissions = {
     'management_team': ['view_client', 'manage_collaborators', 'manage_contracts_creation_modification',
                         'view_contract', 'view_event'],
@@ -37,14 +36,13 @@ permissions = {
 }
 
 for group_name, perm_codenames in permissions.items():
-    print('for ici')
     group = groups[group_name]
-    print(f'for ici pour le group {group}')
     for codename in perm_codenames:
         perm, created = Permission.objects.get_or_create(codename=codename)
         group.permissions.add(perm)
     print(f"Permissions successfully assigned to the group '{group_name}'.")
-    # Function to create a collaborator and add to a group
+
+
 def create_collaborator(first_name, last_name, username, email, role_name, employee_number, password, group_name):
     role, created = Role.objects.get_or_create(name=role_name)
     if created:
@@ -67,7 +65,6 @@ def create_collaborator(first_name, last_name, username, email, role_name, emplo
     print(f"Collaborator '{first_name} {last_name}' created and added to '{group_name}'s group successfully.")
 
 
-# Create users for each role and add them to respective groups
 create_collaborator("Aurelien", "Allenic", "aurelien", "aurelien.allenic@gmail.com",
                     "management", "9473", "managementPassword9473",
                     "management_team")
@@ -77,23 +74,22 @@ create_collaborator("Emma", "Stone", "emmaStone", "emma.stone@example.net",
                     "support", "9475", "SupportPassword9475",
                     "support_team")
 
-# Function to find sales contact by username
+
 def find_sales_contact(username):
     try:
         return Collaborator.objects.get(username=username)
     except Collaborator.DoesNotExist:
-        print(f"No sales contact found with : {username}")
+        print(f"No sales contact found with username: {username}")
         return None
 
 
-# Function to create a client
 def create_client(name, email, phone, company_name, commercial_contact):
     sales_contact = find_sales_contact(commercial_contact)
     if sales_contact is None:
         return
 
     client = Client(
-        name= name,
+        name=name,
         email=email,
         phone=phone,
         company_name=company_name,
@@ -102,8 +98,6 @@ def create_client(name, email, phone, company_name, commercial_contact):
     client.save()
     print(f"Client '{name}' created.")
 
-
-# Create clients and assign their sales contact
 clients_data = [
     {"name": "first client", "email": "first.client@client.com", "phone": "0625324428", "company_name": "first company",
      "commercial_contact": "aurelien"},
@@ -116,22 +110,47 @@ clients_data = [
 for client_data in clients_data:
     create_client(**client_data)
 
-def create_event(name, client_name, client_contact, day_start, date_end, support_contact_username, location, attendees, notes):
-    # Recherche du client par nom
+contracts_data = [
+    {"client_infos": "first client", "commercial_contact": "aurelien", "value": 1000, "due": 200, "status": "open"},
+    {"client_infos": "second client", "commercial_contact": "aurelien", "value": 1500, "due": 300, "status": "open"}
+]
+
+contracts = {}
+for data in contracts_data:
+    try:
+        client = Client.objects.get(name=data["client_infos"])
+        commercial_contact = Collaborator.objects.get(username=data["commercial_contact"])
+    except Client.DoesNotExist:
+        print(f"Client '{data['client_infos']}' not found.")
+        continue
+    except Collaborator.DoesNotExist:
+        print(f"Commercial contact '{data['commercial_contact']}' not found.")
+        continue
+
+    contract = Contract.objects.create(
+        client_infos=client,
+        commercial_contact=commercial_contact,
+        value=data["value"],
+        due=data["due"],
+        status=data["status"]
+    )
+    contracts[data["client_infos"]] = contract
+    print(f"Contract for client '{data['client_infos']}' created successfully.")
+
+
+def create_event(name, client_name, client_contact, day_start, date_end, support_contact_username, location, attendees, notes, contract):
     try:
         client = Client.objects.get(name=client_name)
     except Client.DoesNotExist:
         print(f"Client '{client_name}' not found.")
         return
-    
-    # Recherche du contact de support par nom d'utilisateur
+
     try:
         support_contact = Collaborator.objects.get(username=support_contact_username)
     except Collaborator.DoesNotExist:
         print(f"Support contact '{support_contact_username}' not found.")
         return
 
-    # Création de l'événement
     event = Evenement.objects.create(
         name=name,
         client=client,
@@ -142,11 +161,12 @@ def create_event(name, client_name, client_contact, day_start, date_end, support
         support_contact=support_contact,
         location=location,
         attendees=attendees,
-        notes=notes
+        notes=notes,
+        contract=contract
     )
     print(f"Event '{event}' created successfully.")
     return event
 
-create_event("first event", "first client", "John Doe", date(2024, 5, 10), date(2024, 5, 11), "emmaStone", "Paris", 50, "Meeting with client")
-create_event("second event", "second client", "John Doe", date(2024, 5, 10), date(2024, 5, 11), "emmaStone", "Paris", 50, "Meeting with client")
 
+create_event("first event", "first client", "John Doe", date(2024, 5, 10), date(2024, 5, 11), "emmaStone", "Paris", 50, "Meeting with client", contracts["first client"])
+create_event("second event", "second client", "John Doe", date(2024, 5, 10), date(2024, 5, 11), "emmaStone", "Paris", 50, "Meeting with client", contracts["second client"])
